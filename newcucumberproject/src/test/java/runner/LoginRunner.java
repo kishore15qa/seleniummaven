e86f44296d756021cad1d52efde98b56415a4fa3
Kishore1561999 <rskishore1561999@gmail.com>
2025-12-12 19:19:31 +0530

    New change1

--- newcucumberproject/src/test/java/runner/LoginRunner.java
+++ newcucumberproject/src/test/java/runner/LoginRunner.java
@@ -10,10 +10,10 @@ import io.cucumber.junit.CucumberOptions;
     tags = "@Smoke or @Regression",
     features = "src/test/java/feature", 
     glue = "stepdefinition", 
-    plugin = {"pretty", 
-                "html:target/cucumber-report.html",
-                "json:target/cucumber-report.json",
-                "junit:target/cucumber-report.xml"},
+    plugin = { "pretty",
+    "html:target/cucumber.html",
+    "json:target/cucumber.json",
+    "junit:target/cucumber.xml"},
     monochrome = true)
 public class LoginRunner {
 
